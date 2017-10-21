f2c05152ffc5b88a16dd436d71545634fc58a6a3
spikeyduy <spikeyduy@gmail.com>
2017-10-21 14:55:58 -0500

    Working on fixing the settings and the buttonListener

--- app/src/main/java/edu/uiowa/mdphan/collegefootball/SettingsActivityFragment.java
+++ app/src/main/java/edu/uiowa/mdphan/collegefootball/SettingsActivityFragment.java
@@ -5,20 +5,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class SettingsActivityFragment extends Fragment {
 
-    // Need to get the view
+    // set up the radio buttons
+    private static RadioGroup radioG;
+    private static RadioButton radioButt;
 
     public SettingsActivityFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        radioG = view.findViewById(R.id.choicesGroup);
+        
+
+        return view;
     }
+
+
 }
